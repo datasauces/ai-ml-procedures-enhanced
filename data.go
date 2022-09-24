26842ced36cd869f4952240a5d32ad868bd909d2
datasauces <datasauces@proton.me>
2022-09-24 22:28:26 +0000

    update plague diary

--- data.go
+++ data.go
@@ -32,4 +32,4 @@ func LoadMatrixCSV(filename string) (linear.Matrix, error) {
 			recordLength = len(record)
 			continue // first record is header
 		} else if len(record) != recordLength {
-			return nil, fmt.Errorf("expected record of length %d but found length %d", rec
+			return nil, fmt.Errorf("expected record of length %d but found length %d", recordLength, len(r
