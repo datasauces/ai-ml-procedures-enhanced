fcda9b73e0748f18c7815f54f65ee5b1eef84ade
datasauces <datasauces@proton.me>
2022-10-01 21:22:25 +0000

    displaying polls

--- data.go
+++ data.go
@@ -34,4 +34,4 @@ func LoadMatrixCSV(filename string) (linear.Matrix, error) {
 		} else if len(record) != recordLength {
 			return nil, fmt.Errorf("expected record of length %d but found length %d", recordLength, len(record))
 		}
-		for _, v 
+		for _, v := range record 
