909ae343a7bc74ab5c393d2dfb9fd69894164b13
datasauces <datasauces@proton.me>
2022-12-19 17:13:32 +0000

    Added testcases

--- data.go
+++ data.go
@@ -45,4 +45,5 @@ func LoadMatrixCSV(filename string) (linear.Matrix, error) {
 	if len(values) == 0 {
 		return nil, errors.New("no values found in csv")
 	}
-	ins := recordLeng
+	ins := recordLength
+	outs := len(val
