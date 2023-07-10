2e18c1808116c0716630dfce0cee471f8626dedf
datasauces <datasauces@proton.me>
2023-07-10 19:34:37 +0000

    Clean translations

--- data.go
+++ data.go
@@ -83,4 +83,4 @@ func swapRows(A linear.Matrix, a, b, ins int) {
 func ShuffleRows(A linear.Matrix, order []int) {
 	ins, outs := A.Shape()
 	location := make([]int, outs)
-	for o
+	for o := 0; o < outs
