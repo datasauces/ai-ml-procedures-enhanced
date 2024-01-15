2a330999b926d72ca53385c2677f22deea103f56
datasauces <datasauces@proton.me>
2024-01-15 08:56:36 +0000

    whitespace edit

--- linear_test.go
+++ linear_test.go
@@ -55,4 +55,5 @@ func TestLinearRegressionNonSquare(t *testing.T) {
 
 	ExpectInt(2, theta_hat.Dimension(), t)
 	ExpectFloat(6, theta_hat.Get(0), t)
-	ExpectFloat(-3, theta_ha
+	ExpectFloat(-3, theta_hat.Get(1), t)
+}
