730f6e7150fb30b21ade9fc8fcf5f89594252fe7
datasauces <datasauces@proton.me>
2024-03-08 06:35:22 +0000

    workflow

--- linear_test.go
+++ linear_test.go
@@ -80,4 +80,4 @@ func TestLinearRegressionBoston(t *testing.T) {
 	ExpectFloat(-2.86843637e+00, theta_hat.Get(4), t)
 	ExpectFloat(5.92814778e+00, theta_hat.Get(5), t)
 	ExpectFloat(-7.26933458e-03, theta_hat.Get(6), t)
-	ExpectFloat(-9.68514157e-01, theta_
+	ExpectFloat(-9.68514157e-01, theta_hat.Get(7), t
